604421180d493e40ba67ee6c062dd56e5a6732d7
Sofya Surovneva <ssurovneva@mail.ru>
2025-05-20 15:26:14 +0300

    HW! migrations, models, forloops

--- blog/article/admin.py
+++ blog/article/admin.py
@@ -5,4 +5,4 @@ from . import models
 # models.НашКласс
 @admin.register(models.Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['dt', 'title']
+    list_display = ['dt', 'title', 'user']
--- blog/article/models.py
+++ blog/article/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 
 class Article(models.Model):
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
     title = models.CharField(max_length=64)
     text = models.CharField(max_length=100000)
     dt = models.DateTimeField(default=datetime.now(), blank=True)
+
+    def __str__ (self):
+        return self.title
+
