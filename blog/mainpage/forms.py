f000826db3406157a33843725bb2758894a5f9d0
Sof Sur <ssurovneva@mail.ru>
2025-05-29 22:04:09 +0300

    регистрация/логин

---
+++ blog/mainpage/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class UserRegistrationForm(forms.ModelForm):
+    
+    password = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput
+    )
+
+    password2 = forms.CharField(
+        label='Повторите пароль',
+        widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = {
+            'username': 'Логин',
+            'first_name': 'Имя',
+            'email': 'Электронная почта'
+        }
+
+        help_texts = {
+            "username": "Обязательное поле. Буквы, цифры, символ подчеркивания."
+        }
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Пароли не совпадают')
+        return cd['password2']
+    
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget = forms.PasswordInput)
+
+    class Meta:
+        labels = {
+            'username': 'Логин',
+            'password': 'Пароль'
+        }
+
--- blog/mainpage/views.py
+++ blog/mainpage/views.py
@@ -1,6 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 def index(request):
+    user = None
+    if request.user.is_authenticated:
+        user = request.user
+    context = {
+        'user': user
+    }
     return render(
-        request, 'mainpage/main.html'
+        request, 
+        'mainpage/main.html',
+        context
+    )
+
+from .forms import UserRegistrationForm, UserLoginForm
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(
+                request,
+                'mainpage/register_done.html',
+                {'new_user': new_user}
+            )
+    else:
+        user_form = UserRegistrationForm()
+    return render(
+        request,
+        'mainpage/register.html',
+        {
+            'user_form': user_form
+        }
+    )
+
+from django.contrib import auth
+def loginme(request):
+    if request.method == 'POST':
+        user_form = UserLoginForm(request.POST)
+        if user_form.is_valid():
+            cd = user_form.cleaned_data
+            user = auth.authenticate(
+                username = cd['username'],
+                password = cd['password']
+            )
+            if user is not None:
+                if user.is_active:
+                    auth.login(request, user)
+                return redirect('/')
+            else:
+                return redirect('login')
+    else:
+        user_form = UserLoginForm()
+    return render(
+        request,
+        'mainpage/user_login.html',
+        {
+            'user_form': user_form
+        }
     )
