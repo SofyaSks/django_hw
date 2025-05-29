3c5cf16333a7ef3f61c8918ca08998f268631f4d
Sof Sur <ssurovneva@mail.ru>
2025-05-29 19:39:04 +0300

    urls вдогонку

--- blog/article/urls.py
+++ blog/article/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('1/', views.article, name='article')
-    path('<int:uid>/', views.article, name='from_bd'),
+    path('new/', views.new_article, name='new_article'),
+    path('<int:uid>/', views.article, name='article'),
     ]
